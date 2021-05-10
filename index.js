497b87df7bb332f8f9f75441b058b2c4136bd6d6
satyam-01-byte <satyamsundaram01@gmail.com>
2021-05-10 21:58:36 +0530

    added mongodb

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
 const engine = require("ejs");
+const dotenv = require("dotenv");
+dotenv.config();
 const path = require("path");
 const Patients = require("./models/patients");
-
+const dburl = `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@patients.o0e6i.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/patients", {
+  .connect(dburl, {
     useUnifiedTopology: true,
     useCreateIndex: true,
     useNewUrlParser: true,
+    useFindAndModify: false,
   })
   .then(() => console.log("database connected"))
   .catch((err) => console.log(err));
