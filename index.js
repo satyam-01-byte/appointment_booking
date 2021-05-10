990493e9a41af8f5d97151e91819f8454bd8ea43
satyam-01-byte <satyamsundaram01@gmail.com>
2021-05-11 00:21:24 +0530

    added twilio sms api and updated styles

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const engine = require("ejs");
 const dotenv = require("dotenv");
 dotenv.config();
 const path = require("path");
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
 const Patients = require("./models/patients");
 const dburl = `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@patients.o0e6i.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 app.set("views", path.join(__dirname, "views"));
@@ -27,7 +30,21 @@ app.get("/", (req, res) => {
 
 app.post("/book", async (req, res) => {
   await Patients.insertMany(req.body);
-  res.send("Appointment booked!");
+  const name = req.body.name;
+  const message = `Name: ${req.body.name}, 
+  Mobile: ${req.body.mobile}, 
+  Address: ${req.body.address}, 
+  Gender: ${req.body.gender}, 
+  Age: ${req.body.age}`;
+  client.messages
+    .create({
+      body: message,
+      from: `${process.env.TWILIO_PHONE_NO}`,
+      to: `${process.env.RECEPTIONIST}`,
+    })
+    .then((message) => console.log(message.sid))
+    .catch((err) => console.log(err));
+  res.render("close", { name });
 });
 
 app.listen(3000, () => {
